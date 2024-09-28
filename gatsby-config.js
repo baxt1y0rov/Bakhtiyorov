e9c2c44aa684e464cfe5706d36375dd0e1b7c133
اِسْمَةُ الله بختيرو <139554680+baxt1y0rov@users.noreply.github.com>
2024-09-28 21:32:19 +0500

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Ismatulloh Bakhtiyorov',
-        short_name: 'Brittany Chiang',
+        short_name: 'Ismatulloh Bakhtiyorov',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
